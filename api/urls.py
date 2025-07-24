7dc6b16973d291cf96491198f3559b69164b4d66
songjaewoo <sjw1118@gmail.com>
2025-07-25 00:02:55 +0900

    조회, 수정 API 작업

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from .views import ContactListView, ContactDetailView
 
 urlpatterns = [
     path('contacts/', ContactListView.as_view()),
-    path('contacts/<int:id>/', ContactDetailView.as_view()),
+    path('contacts/<int:id>/', ContactDetailView.as_view())
 ]
